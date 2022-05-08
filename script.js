772e943f787831f1a30690d98cfa732323712cc5
Gladi023 <Gladimar.Villamil@gmail.com>
2022-05-08 19:02:28 -0400

    window confirm added

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+const specialCharacters = "!@#$%^&*()";
+const generateButton = document.getElementById('generate')
+generateButton.addEventListener('click', writePassword);
 
+
+//var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
+
 function writePassword() {
+
+
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
-    password.Text.value = password;
+    passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Window Alerts after click
+function generatePassword() {
+    var passwordLength = prompt("Please enter number of characters between 8 and 128.")
+    var lowerCases = confirm("Do you want Lowercases in your password?");
+    var upperCases = confirm("Do you want uppercases in your password?");
+    var numbers = confirm("Do you want numbers in your password?");
+    var special = confirm("Do you want special characters in your password?");
+
+}
+
