1b00131ad00c784fa93fdcaea1fb70ee50167507
Gladi023 <Gladimar.Villamil@gmail.com>
2022-05-08 23:39:36 -0400

    updated JavaScript and html

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # PassswordGenerator
+README Items:
+Installation
+Usage
+Credits
+License
 
-This commit consists of the test functionality to find the test file.
+
+## Installation
+Deploy the website and navigate through the website.
+
+## Usage
+After deploying the website the user can check on thir password criteria by clicking on the checkboxes as shown below:
+![Screenshot of the website](assets/images/Screen%20Shot%202022-05-08%20at%2011.34.30%20PM.png)
+
+## Credits
+The original material that is included in the referenced websitres reviewed belongs to Trilogy Education Services. 
+
+## License
+MIT License
+
+Copyright (c) [2022] [Gladimar]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,13 +1,56 @@
 // Add event listener to generate button
-const specialCharacters = "!@#$%^&*()";
+//const specialCharacters = "!@#$%^&*()";
 const generateButton = document.getElementById('generate')
-generateButton.addEventListener('click', writePassword);
+function generatePassword() {
+    var passwordLength = prompt("Please enter number of characters between 8 and 128.")
+    generatePassword();
+//generateButton.addEventListener('click', writePassword);
+let lowercase = document.getElementById("lowercase");
+let special = document.getElementById("special");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+
+const specialARR = ["!", "@", "#", "%", "?", "&"];
+const numbersARR = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const lowercaseARR = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+  ]
+const uppercaseARR = lowercaseARR.map((char)=> char.toUpperCase());
+numbersARR.push(10)
+console.log(numbersARR)
+
+const validateCheckBox = () => {
+    let options=[]
+    if (lowercase.checked) {
+        options.push(0)
+    }
+    if (special.checked) {
+        options.push(1)
+
+    }
+    if (uppercase.checked) {
+        options.push(2)
+
+    }
+    if (numbers.checked) {
+        options.push(3)
+
+    }
+
+    function Validate() {
+        if(!validateCheckBox()) {
+            alert("You must check at least one of the checkboxes");
+            return false;
+        }
+        return true
+    
+}
 
 
 //var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
-function writePassword() {
+/*function writePassword() {
 
 
     var password = generatePassword();
@@ -16,8 +59,9 @@ function writePassword() {
     passwordText.value = password;
 }
 
+
 //Window Alerts after click
-function generatePassword() {
+ function generatePassword() {
     var passwordLength = prompt("Please enter number of characters between 8 and 128.")
     var lowerCases = confirm("Do you want Lowercases in your password?");
     var upperCases = confirm("Do you want uppercases in your password?");
@@ -26,3 +70,42 @@ function generatePassword() {
 
 }
 
+var functionArray = {
+    getNumbers: function(){
+        return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+    getLowerCases: function() {
+        return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+    getUpperCases: function() {
+        return String.fromCharCode(Math.floor(Math.random() * 26 +65));
+    },
+    getSpecialCharacters: function() {
+        return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+    }
+};
+
+if (numbers === true) {
+    minimumNumbers = functionArray.getNumbers();
+    minimumCount++;
+}
+
+if (lowerCases === true) {
+    minimumLowerCases = functionArray.getLowerCases();
+    minimumCount++;
+}
+
+if (upperCases === true) {
+    minimumUpperCases = functionArray.getUpperCases();
+    minimumCount++;
+} 
+
+// for loop below
+
+var passwordGenerated = "";
+
+// loop to get the random characters
+for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+    var passwordGenerated = randomNumberPicked;
+}
+*/
